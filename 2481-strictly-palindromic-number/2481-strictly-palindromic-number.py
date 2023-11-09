70bdc9f347cccc2c644311267ad9a6306b4d28a9
somyaranjan99 <bapuni99@gmail.com>
2023-11-09 19:18:58 +0530

    Time: 35 ms (76.73%), Space: 16.2 MB (64.68%) - LeetHub

--- 2481-strictly-palindromic-number/2481-strictly-palindromic-number.py
+++ 2481-strictly-palindromic-number/2481-strictly-palindromic-number.py
@@ -1,4 +1,13 @@
 class Solution:
     def isStrictlyPalindromic(self, n: int) -> bool:
-      return False
+      for i in range(2, n-1):
+            bit_form = ''
+            num = n
+            while num:
+                rem = num % i
+                num = num // i
+                bit_form += str(rem)
+            if bit_form != bit_form[::-1]:
+                return False
+      return True
         
